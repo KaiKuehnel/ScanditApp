f3c6a1bec5d57187f08647481bc88c8b68909470
Harshit Sethi <harshit@cognitiveview.com>
2020-05-05 02:03:12 +1000

    Changed Main Activity Files

--- app/src/main/java/com/example/scanditapptest/MainActivity.java
+++ app/src/main/java/com/example/scanditapptest/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        initializeSettings();
     }
 
     @Override
